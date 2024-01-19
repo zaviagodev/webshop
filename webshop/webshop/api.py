18109191e101b8fca5b4062e0e3efb87cd841487
Nabeel Goraya <itsgoraya@gmail.com>
2024-01-19 10:09:31 +0500

    Update api.py

--- webshop/webshop/api.py
+++ webshop/webshop/api.py
@@ -135,54 +135,53 @@ def edit_product_wish(
 
 @frappe.whitelist(allow_guest=True)
 def sign_up(email: str, full_name: str, redirect_to: str) -> tuple[int, str]:
-	if is_signup_disabled():
-		frappe.throw(_("Sign Up is disabled"), title=_("Not Allowed"))
+    if is_signup_disabled():
+        frappe.throw(_("Sign Up is disabled"), title=_("Not Allowed"))
 
-	user = frappe.db.get("User", {"email": email})
-	if user:
-		if user.enabled:
-			return 0, _("Already Registered")
-		else:
-			return 0, _("Registered but disabled")
-	else:
-		if frappe.db.get_creation_count("User", 60) > 300:
-			frappe.respond_as_web_page(
-				_("Temporarily Disabled"),
-				_(
-					"Too many users signed up recently, so the registration is disabled. Please try back in an hour"
-				),
-				http_status_code=429,
-			)
+    user = frappe.db.get("User", {"email": email})
+    if user:
+        if user.enabled:
+            return 0, _("Already Registered")
+        else:
+            return 0, _("Registered but disabled")
+    else:
+        if frappe.db.get_creation_count("User", 60) > 300:
+            frappe.respond_as_web_page(
+                _("Temporarily Disabled"),
+                _(
+                    "Too many users signed up recently, so the registration is disabled. Please try back in an hour"
+                ),
+                http_status_code=429,
+            )
 
-		from frappe.utils import random_string
+        from frappe.utils import random_string
 
-		user = frappe.get_doc(
-			{
-				"doctype": "User",
-				"email": email,
-				"first_name": escape_html(full_name),
-				"enabled": 1,
-				"new_password": random_string(10),
-				"user_type": "Website User",
-			}
-		)
-		user.flags.ignore_permissions = True
-		user.flags.ignore_password_policy = True
-		user.flags.is_verified  = 1
-		user.insert()
-        
+        user = frappe.get_doc(
+            {
+                "doctype": "User",
+                "email": email,
+                "first_name": escape_html(full_name),
+                "enabled": 1,
+                "new_password": random_string(10),
+                "user_type": "Website User",
+            }
+        )
+        user.flags.ignore_permissions = True
+        user.flags.ignore_password_policy = True
+        user.flags.is_verified  = 1
+        user.insert()
+
         default_role = frappe.db.get_single_value("Portal Settings", "default_role")
-		if default_role:
-			user.add_roles(default_role)
-            
-            
-  
-		api_secret = frappe.generate_hash(length=15)
-		if not user.api_key:
-			api_key = frappe.generate_hash(length=15)
-			user.api_key = api_key
-			user.api_secret = api_secret 
-			user.save()
-			user.reload()
-		token = f"{user.api_key}:{user.get_password('api_secret')}"
-		return {"message":'Logged In',"token":token}
+        if default_role:
+            user.add_roles(default_role)
+        
+        api_secret = frappe.generate_hash(length=15)
+        if not user.api_key:
+            api_key = frappe.generate_hash(length=15)
+            user.api_key = api_key
+            user.api_secret = api_secret 
+            user.save()
+            user.reload()
+
+        token = f"{user.api_key}:{user.get_password('api_secret')}"
+        return {"message": 'Logged In', "token": token}
